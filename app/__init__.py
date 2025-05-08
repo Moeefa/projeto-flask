2f6ac55eeb68eaec341fb0b3866a3a8f8681b1b8
Edie <ediecs@gmail.com>
2025-05-08 18:23:08 -0400

    Reestrutura projeto

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'random_secret_key'
-app.config.from_object('config.Config')
+
+# Mudança importante: alterar a configuração para apontar para o serviço 'database' no Docker
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask:flask@database/flask_docker'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
--- app/config.py
+++ app/config.py
@@ -3,4 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:flask@localhost/flask_docker'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:flask@database/flask_docker'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
