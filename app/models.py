d80dcece74b3d5f5fa0ed707d7129e58651afcfc
Edie <ediecs@gmail.com>
2025-05-15 16:32:32 -0400

    testando

--- app/models.py
+++ app/models.py
@@ -4,3 +4,10 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200), nullable=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    items = db.relationship('Item', backref='category', lazy=True)
