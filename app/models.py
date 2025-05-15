843c4ade7ae636813d99b867dcaa4697aa4c09c7
Edie <ediecs@gmail.com>
2025-05-15 18:24:30 -0400

    Revert "mensagem"
    This reverts commit fe1e4d54da6ab7a4f38fea18e43c4d53d0a3e698.

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from app import db
 
-class Category(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(200), nullable=True)
-    items = db.relationship('Item', backref='category', lazy=True)
-
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200), nullable=True)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    items = db.relationship('Item', backref='category', lazy=True)
