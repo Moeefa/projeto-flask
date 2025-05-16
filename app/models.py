dcdb74dbdaf2b1b262bbbf120678a7ff233d8a41
Edie <ediecs@gmail.com>
2025-05-15 21:23:10 -0400

    Cria model Pessoa

--- app/models.py
+++ app/models.py
@@ -4,3 +4,10 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200), nullable=True)
+
+class Pessoa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=True)
+    telefone = db.Column(db.String(20), nullable=True)
+    endereco = db.Column(db.String(100), nullable=True)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import request, redirect, url_for, render_template
 from app import db
 from . import app
-from app.models import Item
+from app.models import Item, Pessoa
 
 @app.route('/')
 def index():
@@ -35,3 +35,47 @@ def delete(id):
     db.session.delete(item)
     db.session.commit()
     return redirect(url_for('index'))
+
+#Rotas da Model Pessoa
+
+@app.route('/pessoas')
+def pessoa_index():
+    pessoas = Pessoa.query.all()
+    return render_template('pessoa/index.html', pessoas=pessoas)
+
+@app.route('/pessoa/create',  methods=['GET', 'POST'])
+def pessoa_create():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form.get('email')
+        telefone = request.form.get('telefone')
+        endereco = request.form.get('endereco')
+        new_pessoa = Pessoa(
+            nome=nome, 
+            email=email, 
+            telefone=telefone,
+            endereco=endereco
+        )
+        db.session.add(new_pessoa)
+        db.session.commit()
+        return redirect(url_for('pessoa_index')) 
+    return render_template('pessoa/create.html')
+
+@app.route('/pessoa/update/<int:id>', methods=['GET', 'POST'])
+def pessoa_update(id):
+    pessoa = Pessoa.query.get_or_404(id)
+    if request.method == 'POST':
+        pessoa.nome = request.form['nome']
+        pessoa.email = request.form.get('email')
+        pessoa.telefone = request.form.get('telefone')
+        pessoa.endereco = request.form.get('endereco')
+        db.session.commit()
+        return redirect(url_for('pessoa_index'))
+    return render_template('pessoa/update.html', pessoa=pessoa)
+
+@app.route('/pessoa/delete/<int:id>')
+def pessoa_delete(id):
+    pessoa = Pessoa.query.get_or_404(id)
+    db.session.delete(pessoa)
+    db.session.commit()
+    return redirect(url_for('pessoa_index'))
